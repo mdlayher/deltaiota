eb63c1a1249445f9be2c478e61c615d85853ebce
Matt Layher <mdlayher@gmail.com>
2014-10-06 10:46:04 -0400

    api/v0/users_test: break TestListUsers() into TestListUsersNoUsers() and TestListUsersManyUsers()

--- api/v0/users_test.go
+++ api/v0/users_test.go
@@ -13,9 +13,9 @@ import (
 	"github.com/mdlayher/deltaiota/ditest"
 )
 
-// TestListUsers verifies that ListUsers returns the appropriate HTTP status
-// code, body, and any errors which occur.
-func TestListUsers(t *testing.T) {
+// TestListUsersNoUsers verifies that ListUsers returns no users when no
+// users exist in the database.
+func TestListUsersNoUsers(t *testing.T) {
 	// Invoke tests with temporary database
 	err := ditest.WithTemporaryDB(func(db *data.DB) {
 		// Build context
@@ -23,51 +23,103 @@ func TestListUsers(t *testing.T) {
 			db: db,
 		}
 
-		// Loop twice to check for empty users, add a user, then verify
-		// that the same user is returned
-		var lastUser *models.User
-		for i := 0; i < 1; i++ {
-			// Fetch list of current users
-			code, body, err := c.ListUsers(nil, util.Vars{})
-			if err != nil {
+		// Fetch list of current users
+		code, body, err := c.ListUsers(nil, util.Vars{})
+		if err != nil {
+			t.Error(err)
+			return
+		}
+
+		// Ensure proper HTTP status code
+		if code != http.StatusOK {
+			t.Errorf("unexpected code: %v != %v", code, http.StatusOK)
+			return
+		}
+
+		// Unmarshal response body
+		var res UsersResponse
+		if err := json.Unmarshal(body, &res); err != nil {
+			t.Error(err)
+			return
+		}
+
+		// Verify length of users slice
+		if len(res.Users) != 0 {
+			t.Errorf("users slice not empty: %v", res.Users)
+			return
+		}
+	})
+
+	// Check for errors from database setup/cleanup
+	if err != nil {
+		t.Fatalf("ditest.WithTemporaryDB:", err)
+	}
+}
+
+// TestListUserManyUsers verifies that ListUsers returns many users when many users
+// users exist in the database.
+func TestListUsersManyUsers(t *testing.T) {
+	// Invoke tests with temporary database
+	err := ditest.WithTemporaryDB(func(db *data.DB) {
+		// Build context
+		c := &context{
+			db: db,
+		}
+
+		// Generate and save a mock users in the database
+		users := make([]*models.User, 100)
+		for i := range users {
+			user := ditest.MockUser()
+			if err := c.db.InsertUser(user); err != nil {
 				t.Error(err)
 				return
 			}
 
-			// Ensure proper HTTP status code
-			if code != http.StatusOK {
-				t.Errorf("unexpected code: %v != %v", code, http.StatusOK)
-				return
+			users[i] = user
+		}
+
+		// Fetch list of current users
+		code, body, err := c.ListUsers(nil, util.Vars{})
+		if err != nil {
+			t.Error(err)
+			return
+		}
+
+		// Ensure proper HTTP status code
+		if code != http.StatusOK {
+			t.Errorf("unexpected code: %v != %v", code, http.StatusOK)
+			return
+		}
+
+		// Unmarshal response body
+		var res UsersResponse
+		if err := json.Unmarshal(body, &res); err != nil {
+			t.Error(err)
+			return
+		}
+
+		// Check length of response slice
+		if len(users) != len(res.Users) {
+			t.Errorf("unexpected Users slice length: %v != %v", len(users), len(res.Users))
+			return
+		}
+
+		// Check if all generated users returned
+		for i := range res.Users {
+			if res.Users[i].Username != users[i].Username {
+				t.Errorf("unexpected User username: %v != %v", res.Users[i].Username, users[i].Username)
 			}
 
-			// Unmarshal response body
-			var res UsersResponse
-			if err := json.Unmarshal(body, &res); err != nil {
-				t.Error(err)
-				return
+			if res.Users[i].FirstName != users[i].FirstName {
+				t.Errorf("unexpected User first name: %v != %v", res.Users[i].FirstName, users[i].FirstName)
 			}
 
-			// Verify length of users slice
-			if len(res.Users) != i {
-				t.Errorf("unexpected number of users returned: %v", res.Users)
-				return
+			if res.Users[i].LastName != users[i].LastName {
+				t.Errorf("unexpected User last name: %v != %v", res.Users[i].LastName, users[i].LastName)
 			}
 
-			// Check if generated user returned and break loop on second run
-			if i == 1 {
-				if res.Users[0] != lastUser {
-					t.Errorf("unexpected User: %v != %v", res.Users[0], lastUser)
-					return
-				}
-
-				break
-			}
-
-			// Generate and save a mock user in the database
-			lastUser = ditest.MockUser()
-			if err := c.db.InsertUser(lastUser); err != nil {
-				t.Error(err)
-				return
+			if res.Users[i].Email != users[i].Email {
+				t.Errorf("unexpected User email: %v != %v", res.Users[i].Email, users[i].Email)
 			}
 		}
 	})
