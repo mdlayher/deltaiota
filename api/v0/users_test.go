38d762702fd1f9c584b7ec9ed6d0eaf92568de67
Matt Layher <mdlayher@gmail.com>
2014-10-05 23:42:22 -0400

    api/v0/users_test: add TestPutUser

--- api/v0/users_test.go
+++ api/v0/users_test.go
@@ -310,3 +310,156 @@ func TestPostUser(t *testing.T) {
 		t.Fatalf("ditest.WithTemporaryDB:", err)
 	}
 }
+
+// TestPutUser verifies that PutUser returns the appropriate HTTP status
+// code, body, and any errors which occur.
+func TestPutUser(t *testing.T) {
+	// Invoke tests with temporary database
+	err := ditest.WithTemporaryDB(func(db *data.DB) {
+		// Build context
+		c := &context{
+			db: db,
+		}
+
+		// JSON used to generate a temporary user
+		mockUserJSON := []byte(`{"id": 1, "password":"test","firstName":"test","lastName":"test","username":"test","email":"test@test.com"}`)
+
+		// Unmarshal into mock user
+		user := new(models.User)
+		if err := json.Unmarshal(mockUserJSON, user); err != nil {
+			t.Error(err)
+			return
+		}
+
+		// Save user in database, to be updated later
+		if err := c.db.InsertUser(user); err != nil {
+			t.Error(err)
+			return
+		}
+
+		// Save user in database for conflicting username and email
+		conflictUser := &models.User{
+			Username: "conflict",
+			Email:    "conflict@conflict.com",
+		}
+		conflictUser.SetPassword("conflict")
+		if err := c.db.InsertUser(conflictUser); err != nil {
+			t.Error(err)
+			return
+		}
+
+		// Table of tests to iterate
+		var tests = []struct {
+			id         string
+			code       int
+			errMessage string
+			body       []byte
+		}{
+			// Empty ID
+			{"", http.StatusBadRequest, userMissingID, nil},
+			// Bad ID
+			{"-1", http.StatusBadRequest, userInvalidID, nil},
+			{"test", http.StatusBadRequest, userInvalidID, nil},
+			// ID not found
+			{"3", http.StatusNotFound, userNotFound, nil},
+			// Empty body
+			{"1", http.StatusBadRequest, userJSONSyntax, nil},
+			// Bad JSON
+			{"1", http.StatusBadRequest, userJSONSyntax, []byte(`{`)},
+			// Missing password
+			{"1", http.StatusBadRequest, "empty field: password", []byte(`{"password":""}`)},
+			// Missing username
+			{"1", http.StatusBadRequest, "empty field: username", []byte(`{"password":"test","firstName":"test","lastName":"test","email":"test@test.com"}`)},
+			// Missing first name
+			{"1", http.StatusBadRequest, "empty field: firstName", []byte(`{"password":"test","lastName":"test","username":"test","email":"test@test.com"}`)},
+			// Missing last name
+			{"1", http.StatusBadRequest, "empty field: lastName", []byte(`{"password":"test","firstName":"test","username":"test","email":"test@test.com"}`)},
+			// Missing email
+			{"1", http.StatusBadRequest, "empty field: email", []byte(`{"password":"test","firstName":"test","lastName":"test","username":"test"}`)},
+			// Invalid email
+			{"1", http.StatusBadRequest, "invalid field: email (could not parse valid email address)", []byte(`{"password":"test","firstName":"test","lastName":"test","username":"test","email":"test"}`)},
+			// Valid request
+			{"1", http.StatusOK, "", mockUserJSON},
+			// Duplicate username
+			{"1", http.StatusConflict, userConflict, []byte(`{"password":"test","firstName":"test","lastName":"test","username":"conflict","email":"test@test.com"}`)},
+			// Duplicate email
+			{"1", http.StatusConflict, userConflict, []byte(`{"password":"test","firstName":"test","lastName":"test","username":"test","email":"conflict@conflict.com"}`)},
+		}
+
+		// Iterate and run tests
+		for _, test := range tests {
+			// Generate HTTP request
+			r, err := http.NewRequest("PUT", "/", bytes.NewReader(test.body))
+			if err != nil {
+				t.Error(err)
+				return
+			}
+
+			// Set path variables, unless ID is missing
+			vars := util.Vars{}
+			if test.id != "" {
+				vars["id"] = test.id
+			}
+
+			// Invoke PutUser with HTTP request, manually injecting
+			// path variables from test
+			code, body, err := c.PutUser(r, vars)
+			if err != nil {
+				t.Error(err)
+				return
+			}
+
+			// Ensure proper HTTP status code
+			if code != test.code {
+				t.Errorf("unexpected code: %v != %v", code, test.code)
+				return
+			}
+
+			// If code is in HTTP 400 or above, check error response
+			if code >= http.StatusBadRequest {
+				// Unmarshal error JSON into struct
+				var errRes util.ErrorResponse
+				if err := json.Unmarshal(body, &errRes); err != nil {
+					t.Error(err)
+					return
+				}
+
+				// Verify error code and message
+				if errRes.Error.Code != test.code {
+					t.Errorf("unexpected error code: %v != %v", errRes.Error.Code, test.code)
+					return
+				}
+				if errRes.Error.Message != test.errMessage {
+					t.Errorf("unexpected error message: %v != %v", errRes.Error.Message, test.errMessage)
+					return
+				}
+
+				// Skip to next test
+				continue
+			}
+
+			// Unmarshal response body
+			var res UsersResponse
+			if err := json.Unmarshal(body, &res); err != nil {
+				t.Error(err)
+				return
+			}
+
+			// Verify length of users slice
+			if len(res.Users) != 1 {
+				t.Errorf("unexpected number of users returned: %v", res.Users)
+				return
+			}
+
+			// Verify user is the same as the mock we created earlier
+			if !reflect.DeepEqual(user, res.Users[0]) {
+				t.Errorf("unexpected user: %v != %v", user, res.Users[0])
+			}
+		}
+	})
+
+	// Check for errors from database setup/cleanup
+	if err != nil {
+		t.Fatalf("ditest.WithTemporaryDB:", err)
+	}
+}
