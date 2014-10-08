07e29803cf7f2e56f68713708c5e831005796b5c
Matt Layher <mdlayher@gmail.com>
2014-10-08 16:54:46 -0400

    data/sessions: add db.DeleteSession method

--- data/sessions.go
+++ data/sessions.go
@@ -11,6 +11,11 @@ const (
 			, "expire"
 		) VALUES (?, ?, ?);
 	`
+
+	// sqlDeleteSession is the SQL statement used to delete an existing Session
+	sqlDeleteSession = `
+		DELETE FROM sessions WHERE id = ?;
+	`
 )
 
 // InsertSession starts a transaction, inserts a new Session, and attempts to commit
@@ -18,6 +23,14 @@ const (
 func (db *DB) InsertSession(u *models.Session) error {
 	return db.withTx(func(tx *Tx) error {
 		return tx.InsertSession(u)
+	})
+}
+
+// DeleteSession starts a transaction, deletes the input Session by its ID, and attempts
+// to commit the transaction.
+func (db *DB) DeleteSession(s *models.Session) error {
+	return db.withTx(func(tx *Tx) error {
+		return tx.DeleteSession(s)
 	})
 }
 
@@ -38,4 +51,11 @@ func (tx *Tx) InsertSession(u *models.Session) error {
 	// Store generated ID
 	u.ID = uint64(id)
 	return nil
+}
+
+// DeleteSession updates the input Session by its ID, in the context of the
+// current transaction.
+func (tx *Tx) DeleteSession(s *models.Session) error {
+	_, err := tx.Tx.Exec(sqlDeleteSession, s.ID)
+	return err
 }
