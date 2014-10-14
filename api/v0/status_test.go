f41d782b5071538c6884d011547153491f2d2a46
Matt Layher <mdlayher@gmail.com>
2014-10-14 19:28:17 -0400

    api/v0/status_test: use withContext helper

--- api/v0/status_test.go
+++ api/v0/status_test.go
@@ -2,25 +2,18 @@ package v0
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"runtime"
 	"testing"
 
 	"github.com/mdlayher/deltaiota/api/util"
-	"github.com/mdlayher/deltaiota/data"
-	"github.com/mdlayher/deltaiota/ditest"
 )
 
 // TestStatusAPI verifies that StatusAPI correctly routes requests to
 // other Status API handlers, using the input HTTP request.
 func TestStatusAPI(t *testing.T) {
-	// Invoke tests with temporary database
-	err := ditest.WithTemporaryDB(func(db *data.DB) {
-		// Build context
-		c := &Context{
-			db: db,
-		}
-
+	withContext(t, func(c *Context) error {
 		var tests = []struct {
 			method string
 			code   int
@@ -40,83 +33,61 @@ func TestStatusAPI(t *testing.T) {
 			// Generate HTTP request
 			r, err := http.NewRequest(test.method, "/", nil)
 			if err != nil {
-				t.Error(err)
-				return
+				return err
 			}
 
 			// Delegate to appropriate handler
 			code, _, err := c.StatusAPI(r, util.Vars{})
 			if err != nil {
-				t.Error(err)
-				return
+				return err
 			}
 
 			// Ensure proper HTTP status code
 			if code != test.code {
-				t.Errorf("unexpected code: %v != %v", code, test.code)
-				return
+				return fmt.Errorf("unexpected code: %v != %v", code, test.code)
 			}
 		}
-	})
 
-	// Check for errors from database setup/cleanup
-	if err != nil {
-		t.Fatal("ditest.WithTemporaryDB:", err)
-	}
+		return nil
+	})
 }
 
 // TestGetStatus verifies that GetStatus returns accurate server status values.
 func TestGetStatus(t *testing.T) {
-	// Invoke tests with temporary database
-	err := ditest.WithTemporaryDB(func(db *data.DB) {
-		// Build context
-		c := &Context{
-			db: db,
-		}
-
+	withContext(t, func(c *Context) error {
 		// Fetch server status
 		code, body, err := c.GetStatus(nil, util.Vars{})
 		if err != nil {
-			t.Error(err)
-			return
+			return err
 		}
 
 		// Ensure proper HTTP status code
 		if code != http.StatusOK {
-			t.Errorf("unexpected code: %v != %v", code, http.StatusOK)
-			return
+			return fmt.Errorf("unexpected code: %v != %v", code, http.StatusOK)
 		}
 
 		// Unmarshal response body
 		var res StatusResponse
 		if err := json.Unmarshal(body, &res); err != nil {
-			t.Error(err)
-			return
+			return err
 		}
 
 		// Verify status returned
 		if res.Status == nil {
-			t.Error("empty Status object in response")
-			return
+			return fmt.Errorf("empty Status object in response")
 		}
 
 		// Verify that some static values are the same
 		if res.Status.Architecture != runtime.GOARCH {
-			t.Errorf("unexpected Architecture: %v != %v", res.Status.Architecture, runtime.GOARCH)
-			return
+			return fmt.Errorf("unexpected Architecture: %v != %v", res.Status.Architecture, runtime.GOARCH)
 		}
 		if res.Status.NumCPU != runtime.NumCPU() {
-			t.Errorf("unexpected NumCPU: %v != %v", res.Status.NumCPU, runtime.NumCPU())
-			return
+			return fmt.Errorf("unexpected NumCPU: %v != %v", res.Status.NumCPU, runtime.NumCPU())
 		}
 		if res.Status.Platform != runtime.GOOS {
-			t.Errorf("unexpected Platform: %v != %v", res.Status.Platform, runtime.GOOS)
-			return
+			return fmt.Errorf("unexpected Platform: %v != %v", res.Status.Platform, runtime.GOOS)
 		}
-	})
 
-	// Check for errors from database setup/cleanup
-	if err != nil {
-		t.Fatal("ditest.WithTemporaryDB:", err)
-	}
+		return nil
+	})
 }
