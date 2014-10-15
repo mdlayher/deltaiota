f4715aeb4f1520a44635c6c00125ddc72c6ae7e2
Matt Layher <mdlayher@gmail.com>
2014-10-15 09:33:57 -0400

    api/auth/auth_test: refactor monolithic Test_makeAuthHandler into individual, single-function tests

--- api/auth/auth_test.go
+++ api/auth/auth_test.go
@@ -3,6 +3,7 @@ package auth
 import (
 	"bytes"
 	"errors"
+	"fmt"
 	"log"
 	"net/http"
 	"net/http/httptest"
@@ -14,38 +15,73 @@ import (
 	"github.com/mdlayher/deltaiota/ditest"
 )
 
-// Test_makeAuthHandler ensures that makeAuthHandler generates the appropriate
-// http.HandlerFunc for an input AuthenticateFunc and http.HandlerFunc.
-func Test_makeAuthHandler(t *testing.T) {
+// Test_makeAuthHandler_ClientError verifies that makeAuthHandler generates
+// appropriate output for a client error.
+func Test_makeAuthHandler_ClientError(t *testing.T) {
 	// Test function which returns a formatted client error
+	reason := "foo bar"
 	clientErrFn := func(r *http.Request) (*models.User, *models.Session, error, error) {
 		return nil, nil, &Error{
-			Reason: "foo bar",
+			Reason: reason,
 		}, nil
 	}
 
+	// Build client failure JSON
+	authFailJSON := []byte(`{"error":{"code":401,"message":"authentication failed: ` + reason + `"}}`)
+
+	// Perform test
+	if err := makeAuthHandlerTest(clientErrFn, okHandler(), http.StatusUnauthorized, authFailJSON, nil); err != nil {
+		t.Fatal(err)
+	}
+}
+
+// Test_makeAuthHandler_ClientNonStandardError verifies that makeAuthHandler
+// generates a generic error when the wrapped Error type is not used.
+func Test_makeAuthHandler_ClientNonStandardError(t *testing.T) {
 	// Test function which returns a non-standard client error
 	clientBadErrFn := func(r *http.Request) (*models.User, *models.Session, error, error) {
 		return nil, nil, errors.New("some error"), nil
 	}
 
+	// Perform test
+	if err := makeAuthHandlerTest(clientBadErrFn, okHandler(), http.StatusUnauthorized, util.JSON[util.NotAuthorized], nil); err != nil {
+		t.Fatal(err)
+	}
+}
+
+// Test_makeAuthHandler_ServerError verifies that makeAuthHandler
+// generates appropriate server error output and logging when an internal
+// server error occurs.
+func Test_makeAuthHandler_ServerError(t *testing.T) {
 	// Test function which returns a server error
 	errServer := errors.New("internal server error")
 	serverErrFn := func(r *http.Request) (*models.User, *models.Session, error, error) {
 		return nil, nil, nil, errServer
 	}
 
+	// Perform test
+	if err := makeAuthHandlerTest(serverErrFn, okHandler(), http.StatusInternalServerError, util.JSON[util.InternalServerError], errServer); err != nil {
+		t.Fatal(err)
+	}
+}
+
+// Test_makeAuthHandler_NoError verifies that makeAuthHandler allows a
+// HTTP handler to proceed with no authentication context.
+func Test_makeAuthHandler_NoError(t *testing.T) {
 	// Test function which returns OK
 	okFn := func(r *http.Request) (*models.User, *models.Session, error, error) {
 		return nil, nil, nil, nil
 	}
 
-	// Test handler called on successful authentication
-	handlerFn := func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("hello world"))
+	// Perform test
+	if err := makeAuthHandlerTest(okFn, okHandler(), http.StatusOK, []byte("hello world"), nil); err != nil {
+		t.Fatal(err)
 	}
+}
 
+// Test_makeAuthHandler_UserSessionContext verifies that makeAuthHandler allows
+// a HTTP handler to proceed with a user and session authentication context.
+func Test_makeAuthHandler_UserSessionContext(t *testing.T) {
 	// Test function which stores some request context
 	user := ditest.MockUser()
 	session, err := user.NewSession(time.Now().Add(1 * time.Minute))
@@ -65,66 +101,9 @@ func Test_makeAuthHandler(t *testing.T) {
 		w.Write([]byte(cUser.Username + cSession.Key))
 	}
 
-	// Build JSON response for client errors
-	authFailJSON := func(reason string) []byte {
-		return []byte(`{"error":{"code":401,"message":"authentication failed: ` + reason + `"}}`)
-	}
-
-	var tests = []struct {
-		fn   AuthenticateFunc
-		h    http.HandlerFunc
-		code int
-		body []byte
-		err  error
-	}{
-		// Client error
-		{clientErrFn, handlerFn, http.StatusUnauthorized, authFailJSON("foo bar"), nil},
-		// Non-standard client error
-		{clientBadErrFn, handlerFn, http.StatusUnauthorized, util.JSON[util.NotAuthorized], nil},
-		// Server error
-		{serverErrFn, handlerFn, http.StatusInternalServerError, util.JSON[util.InternalServerError], errServer},
-		// No error
-		{okFn, handlerFn, http.StatusOK, []byte("hello world"), nil},
-		// No error, with context
-		{contextFn, contextHandlerFn, http.StatusOK, []byte(user.Username + session.Key), nil},
-	}
-
-	for _, test := range tests {
-		// Capture log output in buffer
-		buffer := bytes.NewBuffer(nil)
-		log.SetOutput(buffer)
-
-		// Create mock request
-		r, err := http.NewRequest("GET", "/", nil)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		// Invoke auth handler and capture output
-		w := httptest.NewRecorder()
-		makeAuthHandler(test.fn, test.h).ServeHTTP(w, r)
-
-		// Verify expected code
-		if w.Code != test.code {
-			t.Fatalf("unexpected code: %v != %v", w.Code, test.code)
-		}
-
-		// Verify expected body
-		if !bytes.Equal(w.Body.Bytes(), test.body) {
-			t.Fatalf("unexpected body: %v != %v", string(w.Body.Bytes()), string(test.body))
-		}
-
-		// Continue if status OK
-		if w.Code == http.StatusOK {
-			continue
-		}
-
-		// Check for error body from server
-		if test.err != nil {
-			if !bytes.Contains(buffer.Bytes(), []byte(test.err.Error())) {
-				t.Fatal("error not logged:", test.err)
-			}
-		}
+	// Perform test
+	if err := makeAuthHandlerTest(contextFn, contextHandlerFn, http.StatusOK, []byte(user.Username+session.Key), nil); err != nil {
+		t.Fatal(err)
 	}
 }
 
@@ -167,5 +146,55 @@ func Test_basicCredentials(t *testing.T) {
 		if password != test.password {
 			t.Fatalf("unexpected password: %v != %v", password, test.password)
 		}
+	}
+}
+
+// makeAuthHandlerTest accepts input parameters and expected results for
+// makeAuthHandler, and ensures it behaves as expected.
+func makeAuthHandlerTest(fn AuthenticateFunc, h http.HandlerFunc, code int, body []byte, expErr error) error {
+	// Capture log output in buffer
+	buffer := bytes.NewBuffer(nil)
+	log.SetOutput(buffer)
+
+	// Create mock request
+	r, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		return err
+	}
+
+	// Invoke auth handler and capture output
+	w := httptest.NewRecorder()
+	makeAuthHandler(fn, h).ServeHTTP(w, r)
+
+	// Verify expected code
+	if w.Code != code {
+		return fmt.Errorf("unexpected code: %v != %v", w.Code, code)
+	}
+
+	// Verify expected body
+	if !bytes.Equal(w.Body.Bytes(), body) {
+		return fmt.Errorf("unexpected body: %v != %v", string(w.Body.Bytes()), string(body))
+	}
+
+	// End test if status OK
+	if w.Code == http.StatusOK {
+		return nil
+	}
+
+	// Check for error body from server
+	if expErr != nil {
+		if !bytes.Contains(buffer.Bytes(), []byte(expErr.Error())) {
+			return fmt.Errorf("error not logged: %v", expErr)
+		}
+	}
+
+	return nil
+}
+
+// Test handler called on successful authentication
+func okHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("hello world"))
 	}
 }
